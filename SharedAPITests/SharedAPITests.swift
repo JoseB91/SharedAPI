4bc5dfd5beb956a41edf3ff626ce822a853c7772
Jose Briones <jose.briones.r@hotmail.com>
2025-02-20 21:36:22 -0500

    Initial Commit

---
+++ SharedAPITests/SharedAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  SharedAPITests.swift
+//  SharedAPITests
+//
+//  Created by José Briones on 20/2/25.
+//
+
+import Testing
+@testable import SharedAPI
+
+struct SharedAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
